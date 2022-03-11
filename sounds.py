8bc421990a7bec0e91f733ec390957a977644300
Ming Senn Teo <71879120+MSTeo01@users.noreply.github.com>
2022-03-11 13:09:07 -0800

    Update sounds.py

--- sounds.py
+++ sounds.py
@@ -1,26 +1,21 @@
 import pygame
 
-# eat_Sound = pygame.mixer.Sound("./Game Music/Eat Sound.wav")
-# die_Sound = pygame.mixer.Sound("./Game Music/Game Over.wav")
-# game_Music = pygame.mixer.Sound("./Game Music/Snake_Game.mp3")
+eat_Sound = pygame.mixer.Sound("/Game Music/Eat Sound.wav")
+die_Sound = pygame.mixer.Sound("/Game Music/Game Over.wav")
+game_Music = pygame.mixer.Sound("/Game Music/Snake_Game.mp3")
 
-# class Sounds:
-#     happens = False
-#     def __init__(self):
-#         pygame.mixer.music.load(game_Music)
-#         pygame.mixer.music.play(-1)
+class Sounds:
+    happens = False
+    def __init__(self):
+        pygame.mixer.init()
+        pygame.mixer.music.load(game_Music)
+        pygame.mixer.music.play(-1)
+    
+    def eat(self):
+        pygame.mixer.music.load(eat_Sound)
+        pygame.mixer.music.play(0)
+    
+    def die(self):
+        pygame.mixer.music.load(die_Sound)
+        pygame.mixer.music.play(0)
 
-#     if happens:
-#         pygame.mixer.music.load(eat_Sound)
-#         pygame.mixer.music.play(0)
-
-#     if happens:
-#         pygame.mixer.music.load(die_Sound)
-#         pygame.mixer.music.play(0)
-
-if __name__ == "__main__":
-    pygame.init()
-    pygame.mixer.init()
-
-    pygame.mixer.music.load('./Snake_Game.mp3')
-    pygame.mixer.music.play(-1)
