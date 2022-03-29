3842794b0fc2cfe47ae9ad6cd3857834ab52c336
Ming Senn Teo <71879120+MSTeo01@users.noreply.github.com>
2022-03-29 11:37:48 -0700

    Update sounds.py

--- sounds.py
+++ sounds.py
@@ -1,21 +1,34 @@
 import pygame
 
-eat_Sound = pygame.mixer.Sound("/Game Music/Eat Sound.wav")
-die_Sound = pygame.mixer.Sound("/Game Music/Game Over.wav")
-game_Music = pygame.mixer.Sound("/Game Music/Snake_Game.mp3")
+pygame.mixer.init()
+
+eat_Sound = pygame.mixer.Sound("./Game Music/Eat Sound.wav")
+die_Sound = pygame.mixer.Sound("./Game Music/Game Over.wav")
+game_Music = pygame.mixer.Sound("./Game Music/Snake_Game.mp3")
+hover = pygame.mixer.Sound("./Game Music/hover sound.wav")
+click = pygame.mixer.Sound("./Game Music/click sound.wav")
 
 class Sounds:
-    happens = False
-    def __init__(self):
-        pygame.mixer.init()
-        pygame.mixer.music.load(game_Music)
-        pygame.mixer.music.play(-1)
-    
-    def eat(self):
-        pygame.mixer.music.load(eat_Sound)
-        pygame.mixer.music.play(0)
-    
-    def die(self):
-        pygame.mixer.music.load(die_Sound)
-        pygame.mixer.music.play(0)
+	def __init__(self):
+		pass
+
+	def game_music(self):
+		pygame.mixer.music.load(game_Music)
+		pygame.mixer.music.play(-1)
+		
+	def eat(self):
+		pygame.mixer.music.load(eat_Sound)
+		pygame.mixer.music.play(0)
+	
+	def die(self):
+		pygame.mixer.music.load(die_Sound)
+		pygame.mixer.music.play(0)
+
+	def hover(self):
+		pygame.mixer.music.load(hover)
+		pygame.mixer.music.play(0)
+
+	def click(self):
+		pygame.mixer.music.load(click)
+		pygame.mixer.music.play(0)
 
